70ec43b36aa56b661245adc14707ad2840e3ac1b
North-West-Wind <wsyn148@gmail.com>
2026-06-30 11:32:22 +0800

    build: CI settings

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,10 +1,11 @@
+val IS_CI = System.getenv("CI") == "true"
+
 plugins {
     id("dev.kikugie.stonecutter")
     id("net.neoforged.moddev") version "2.0.141" apply false
     id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT" apply false
     id("net.fabricmc.fabric-loom-remap") version "1.17-SNAPSHOT" apply false
 }
-stonecutter active "26.2"
 
 stonecutter {
     parameters {
@@ -13,3 +14,6 @@ stonecutter {
         }
     }
 }
+
+if (IS_CI) stonecutter active null
+else stonecutter active "26.2"
