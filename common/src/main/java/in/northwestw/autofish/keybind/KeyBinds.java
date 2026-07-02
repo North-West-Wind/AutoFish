462d2b6919fc79750641acccbf07571126c9c636
North-West-Wind <wsyn148@gmail.com>
2026-07-02 10:37:41 +0800

    fix: translation keys

--- common/src/main/java/in/northwestw/autofish/keybind/KeyBinds.java
+++ common/src/main/java/in/northwestw/autofish/keybind/KeyBinds.java
@@ -31,10 +31,10 @@ public class KeyBinds {
         KeyMapping.Category cat = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(AutoFish.MOD_ID, "autofish"));
         //? } else
         //String cat = "key.categories.autofish";
-        autofish = new KeyMapping(AutoFish.getTranslatableComponent("key.forgeautofish.autofish").getString(), KEY_MINUS, cat);
-        rodprotect = new KeyMapping(AutoFish.getTranslatableComponent("key.forgeautofish.rodprotect").getString(), KEY_BACKSLASH, cat);
-        autoreplace = new KeyMapping(AutoFish.getTranslatableComponent("key.forgeautofish.autoreplace").getString(), KEY_RIGHT_BRACKET, cat);
-        settings = new KeyMapping(AutoFish.getTranslatableComponent("key.forgeautofish.settings").getString(), KEY_K, cat);
-        itemfilter = new KeyMapping(AutoFish.getTranslatableComponent("key.forgeautofish.itemfilter").getString(), KEY_APOSTROPHE, cat);
+        autofish = new KeyMapping(AutoFish.getTranslatableComponent("key.autofish.autofish").getString(), KEY_MINUS, cat);
+        rodprotect = new KeyMapping(AutoFish.getTranslatableComponent("key.autofish.rodprotect").getString(), KEY_BACKSLASH, cat);
+        autoreplace = new KeyMapping(AutoFish.getTranslatableComponent("key.autofish.autoreplace").getString(), KEY_RIGHT_BRACKET, cat);
+        settings = new KeyMapping(AutoFish.getTranslatableComponent("key.autofish.settings").getString(), KEY_K, cat);
+        itemfilter = new KeyMapping(AutoFish.getTranslatableComponent("key.autofish.itemfilter").getString(), KEY_APOSTROPHE, cat);
     }
 }
